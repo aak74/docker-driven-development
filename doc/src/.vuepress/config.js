340dae442346e574e357f1d56e6cd4a25622462d
Andrew Kopylov <riu@inbox.ru>
2018-09-16 11:45:01 +0300

    Добавлена история и недостатки

--- doc/src/.vuepress/config.js
+++ doc/src/.vuepress/config.js
@@ -1,9 +1,3 @@
-// const mermaid = require('markdown-it-mermaid');
-// const mermaid = require('markdown-it-mermaid-real');
-// const mermaid = require('markdown-it-mermaid');
-// import mermaid from 'markdown-it-mermaid-real';
-// console.log(mermaid.default);
-
 module.exports = {
   themeConfig: {
     sidebar: [
@@ -12,47 +6,17 @@ module.exports = {
         collapsable: false,
         children: [
           '/',
-          '/how-to-deploy/',
-          '/how-to-install/',
-          '/database/',
-          '/amqp/',
-        ]
-      },
-      {
-        title: 'Основные сервисы',
-        collapsable: false,
-        children: [
-          '/web-server/getData',
-          '/get-data/',
-          '/updater/',
-          '/web-server/',
-          '/web-server/auth',
-          '/web-server/updated',
-          '/web-server/update-schedule',
-        ]
-      },
-      {
-        title: 'Служебные сервисы',
-        collapsable: false,
-        children: [
-          '/monitoring/',
-          '/admin/',
-          '/doc/',
-          '/logger/',
-          '/cleaner/',
-          '/cleaner/files/',
-          '/cleaner/database/',
+          '/history/',
+          '/weaknesses/',
+          '/examples/',
         ]
       },
     ],
     sidebarDepth: 3
   },
-  markdown: {
-    config: md => {
-      // md.use(mermaid.default)
-      md.use(require('markdown-it-plantuml'))
-
-      // md.use('../node_modules/markdown-it-mermaid')
-    }
-  }
+  // markdown: {
+  //   config: md => {
+  //     md.use(require('markdown-it-plantuml'))
+  //   }
+  // }
 }
